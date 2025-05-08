a8eab888276c7fcb827910debc3f9c5a97275e14
W.A.Charith Jeewaka <wickramasinghajeewaka@gmail.com>
2025-05-08 06:15:43 +0530

    before another desaster plant

--- src/main/java/lk/ijse/florist_pos/final_project/controller/PlantPageController.java
+++ src/main/java/lk/ijse/florist_pos/final_project/controller/PlantPageController.java
@@ -1,4 +1,24 @@
 package lk.ijse.florist_pos.final_project.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class PlantPageController {
+    public void searchPlantOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deletePlantOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updatePlantOnAction(ActionEvent actionEvent) {
+    }
+
+    public void resetPlantPageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void savePlantOnAction(ActionEvent actionEvent) {
+    }
+
+    public void onClickTable(MouseEvent mouseEvent) {
+    }
 }
